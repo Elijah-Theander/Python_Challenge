3bb657819db8b9a0bebd55cd4d10e3547b316baa
Elijah <elijahtheander@gmail.com>
2022-10-06 23:33:18 -0500

    Add challenge skeleton.py, ch01.py/txt, and python virtual environment.

---
+++ Challenges/ch01.py
@@ -0,0 +1,70 @@
+"""
+ch01.py
+
+Python Challenge 1:  Translate String using a map
+
+Elijah Theander
+October 6, 2022
+
+
+"""
+
+import string
+
+def challenge(num):
+    startingURL = 'http://www.pythonchallenge.com/pc/def/map.html'
+    print(f"--------------------------------------------------------------------------------\n"
+    "Challenge # {num}: Translate a String by using a map.\n"
+    "Starting url: {startingURL}\n"
+    "The image on this page shows a map of letters.\n"
+    "K->M, O-Q, E->G\n"
+    "So it would appear that the map for each letter is the letter two spaces to the\n"
+    "right of it, alphabetically. I am going to assume Y and Z will wrap around and\n"
+    " map to A and B."
+    "--------------------------------------------------------------------------------\n"
+     )
+
+    with open('ch01.txt', 'r') as data:
+
+        clue = ''.join(data.read())
+
+        alphabet = list(string.ascii_lowercase)
+        map = {}
+
+        for i in range(len(alphabet)):
+            if(i == 25): #Z
+                map[alphabet[i]] = 'b'
+            elif(i == 24): #Y
+                map[alphabet[i]] = 'a'
+            else:
+                map[alphabet[i]] = alphabet[i + 2]
+        
+        print('Created Map.\n')
+        
+        trans = clue.maketrans(map)
+        newClue = clue.translate(trans)
+
+        print(f'Translated Clue: {newClue}\n')
+
+        transURL = startingURL.translate(trans)
+
+        print(f'Translated URL: {transURL}\n')
+        #I think I only need to change the portion of the URL after 'def', because otherwise this is total nonsense.
+    next = 'ocr'
+    nextURL = f'http://www.pythonchallenge.com/pc/def/{next}.html\n'
+    print(f'Next challenge URL: {nextURL}')
+    
+
+
+
+def main():
+    print(f"--------------------------------------------------------------------------------\n"
+    "                               PYTHON CHALLENGE\n"
+    "--------------------------------------------------------------------------------\n"
+    )
+
+    challenge(1)
+    
+
+if __name__ == "__main__":
+    main()
--- Challenges/challenge_skeleton.py
+++ Challenges/challenge_skeleton.py
@@ -2,6 +2,7 @@
 challenge_skeleton.py
 
 Skeleton file for Python Challenges
+Also serves as the introductory Challenge 0.
 
 Elijah Theander
 October 6, 2022
@@ -10,17 +11,26 @@ October 6, 2022
 """
 
 def challenge(num):
-    print(f"""
-    ----------------------------------------\n
-    Challenge # {num}: \n
-    ----------------------------------------\n"""
-    )
+    startingURL = 'http://www.pythonchallenge.com/pc/def/0.html'
+    print(f"--------------------------------------------------------------------------------\n"
+    "Challenge # {num}: 2 to the power of 38.\n"
+    "Starting url: {startingURL}\n"
+    "--------------------------------------------------------------------------------\n"
+     )
+
+    power2 = 2**38
+    print(f"2**38: {power2}")
+
+    nextURL = f'http://www.pythonchallenge.com/pc/def/{power2}.html\n'
+    print(f'Next challenge URL: {nextURL}')
+    
+
+
 
 def main():
-    print(f"""
-    ----------------------------------------\n
-               PYTHON CHALLENGE\n
-    ----------------------------------------\n"""
+    print(f"--------------------------------------------------------------------------------\n"
+    "                               PYTHON CHALLENGE\n"
+    "--------------------------------------------------------------------------------\n"
     )
 
     challenge(0)
