9bca02bc5de5c0ecb528b6512566b0f919599c80
Elijah <elijahtheander@gmail.com>
2022-10-06 22:41:45 -0500

    Add Skeleton File

---
+++ Challenges/challenge_skeleton.py
@@ -0,0 +1,30 @@
+"""
+challenge_skeleton.py
+
+Skeleton file for Python Challenges
+
+Elijah Theander
+October 6, 2022
+
+
+"""
+
+def challenge(num):
+    print(f"""
+    ----------------------------------------\n
+    Challenge # {num}: \n
+    ----------------------------------------\n"""
+    )
+
+def main():
+    print(f"""
+    ----------------------------------------\n
+               PYTHON CHALLENGE\n
+    ----------------------------------------\n"""
+    )
+
+    challenge(0)
+    
+
+if __name__ == "__main__":
+    main()
